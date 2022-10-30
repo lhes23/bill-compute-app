3be8441daa1aaf9d5f6275d4f9fa4ee20f820892
Lester <32542078+lhes23@users.noreply.github.com>
2022-10-30 15:11:02 +0800

    add columns to reading

--- reading/models.py
+++ reading/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from django.db import models
 
 class House(models.Model):
@@ -17,6 +18,18 @@ class Tenant(models.Model):
 class Reading(models.Model):
     house_id = models.ForeignKey(House,on_delete=models.CASCADE)
     tenant_id = models.ForeignKey(Tenant, on_delete=models.DO_NOTHING)
+    bill_type = models.CharField(max_length=255, blank=True, null=True)
+    due_date = models.DateField(auto_now_add=True,blank=True, null=True)
+    start_date = models.DateField(auto_now_add=True,blank=True, null=True)
+    end_date = models.DateField(auto_now_add=True,blank=True, null=True)
+    previous_reading = models.IntegerField(blank=True, null=True)
+    present_reading = models.IntegerField(blank=True, null=True)
+    consumption = models.IntegerField(blank=True, null=True)
+    peso_per_consumption = models.IntegerField(blank=True, null=True)
+    bill = models.IntegerField(blank=True, null=True)
+    status = models.CharField(max_length=255, blank=True, null=True)
+    created_at = models.DateField(auto_now_add=True,blank=True, null=True)
+    paid_at = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return str(self.house_id)
